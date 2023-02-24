367009865966fa8816ca03a782416534c03af330
Rana Dudu Kabak <77778044+ranadudukabak@users.noreply.github.com>
2023-02-24 23:54:27 +0300

    Add files via upload

---
+++ hafta-1-ders-1/multiplication-with-infinite-variable.js
@@ -0,0 +1,20 @@
+function printArguments(...args) {
+    let sum = 1;
+    let flag = 0;
+
+    args.forEach((arg, index) => {
+        if (isNaN(arg) == 0) {
+            sum *= arg;
+        } else {
+            flag = 1;
+        }
+    });
+
+    if (flag == 0) {
+        console.log(sum);
+    } else {
+        console.log("Please enter valid values!");
+    }
+}
+
+printArguments(1, 2, 3, 4, "rana", 6); //you can add infinite variables here.
